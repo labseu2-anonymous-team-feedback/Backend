e8cea175327b7334a36288e1b0c5377539b6d576
PascalUlor <pascalulor@yahoo.com>
2019-08-28 10:51:05 +0100

    test google oauth debug google authentication for front end

--- auth/passportConfig.js
+++ auth/passportConfig.js
@@ -19,7 +19,8 @@ passport.use(
   new GoogleTokenStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:4000'
     },
     GoogleTokenStrategyCallback
   )
