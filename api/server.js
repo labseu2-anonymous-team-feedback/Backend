65ee66bee77270edc39cc2e7d5448b85fe18febf
PascalUlor <pascalulor@yahoo.com>
2019-08-29 14:03:52 +0100

    feat redirect to frontend on google authentication

--- api/server.js
+++ api/server.js
@@ -26,8 +26,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        process.env.FE_URL || process.env.URL || 'http://localhost:4000/google'
+      callbackURL: process.env.URL || 'http://localhost:4000/google'
     },
     (accessToken, refreshToken, profile, done) => {
       done(null, {
