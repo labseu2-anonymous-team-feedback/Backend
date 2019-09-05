9298dd4ef6e25d0b8603279abae8936dadc3f8e0
Awa Melvine <melvineawa@gmail.com>
2019-09-05 13:24:27 +0100

    feat(feedback): Create feedback type defs

--- schema/index.js
+++ schema/index.js
@@ -1,5 +1,6 @@
 const { mergeTypes } = require('merge-graphql-schemas');
 const userTypes = require('./user');
 const surveyTypes = require('./survey');
+const feedbackTypes = require('./feedback');
 
-module.exports = mergeTypes([userTypes, surveyTypes]);
+module.exports = mergeTypes([userTypes, surveyTypes, feedbackTypes]);
